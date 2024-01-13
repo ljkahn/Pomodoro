98a02b3b0f60dd564d2abafb400093121e0dfeec
ljkahn <lia.j.kahn@gmail.com>
2024-01-13 14:30:29 -0700

    added timer button and timer/break functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 import Pomodoro from "./pages/Pomodoro";
-
+import Error from "./pages/Error";
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Pomodoro />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </Router>
   );
--- src/pages/Pomodoro.jsx
+++ src/pages/Pomodoro.jsx
@@ -1,32 +1,53 @@
 import React, { useState, useEffect } from "react";
 
 function Pomodoro() {
-  const countdownTimer = () => {
-    const initialTime = 25 * 60;
-    const [time, setTime] = useState(initialTime);
+  const [time, setTime] = useState(25 * 60);
+  const [isActive, setIsActive] = useState(false);
+  const [isBreak, setIsBreak] = useState(false);
 
-    const formatTime = (seconds) => {
-      const minutes = Math.floor(seconds / 60);
-      const remainingSeconds = seconds % 60;
-      return `${String(minutes).padStart(2, "0")}:${String(
-        remainingSeconds
-      ).padStart(2, "0  ")}`;
-    };
-
-    useEffect(() => {
-      const intervalId = setInterval(() => {
-        setTime((prevTime) => (prevTime > 0 ? prevTime - 1 : 0));
-      }, 1000);
-
-      return () => clearInterval(intervalId);
-    }, []);
+  const formatTime = (seconds) => {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    return `${String(minutes).padStart(2, "0")}:${String(
+      remainingSeconds
+    ).padStart(2, "0")}`;
   };
 
-  return;
-  <div>
-    <h1>Countdown Timer</h1>
-    <p>{formatTime(time)}</p>
-  </div>;
+  const startTimer = () => {
+    setIsActive(true);
+  };
+
+  useEffect(() => {
+    let intervalId;
+    if (isActive) {
+      intervalId = setInterval(() => {
+        setTime((prevTime) => (prevTime > 0 ? prevTime - 1 : 0));
+      }, 1000);
+    }
+
+    if (time === 0) {
+      setIsActive(false);
+      if (!isBreak) {
+        setTime(5 * 60);
+      } else {
+        setTime(25 * 60);
+      }
+      setIsBreak(!isBreak);
+    }
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [isActive, isBreak, time]);
+
+  return (
+    <div>
+      <h1>Countdown Timer</h1>
+      <p>{formatTime(time)}</p>
+      <button onClick={startTimer} disabled={isActive}>
+        Start Timer
+      </button>
+    </div>
+  );
 }
 
 export default Pomodoro;
