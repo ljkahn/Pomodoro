876557fdc74f1bcddf3bcc84c23ff5d14a9a7742
ljkahn <lia.j.kahn@gmail.com>
2024-01-13 12:22:27 -0700

    started code for timer

--- src/pages/Pomodoro.jsx
+++ src/pages/Pomodoro.jsx
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function Pomodoro() {
+  const countdownTimer = () => {
+    const initialTime = 25 * 60;
+    const [time, setTime] = useState(initialTime);
+
+    const formatTime = (seconds) => {
+      const minutes = Math.floor(seconds / 60);
+      const remainingSeconds = seconds % 60;
+      return `${String(minutes).padStart(2, "0")}:${String(
+        remainingSeconds
+      ).padStart(2, "0  ")}`;
+    };
+
+    useEffect(() => {
+      const intervalId = setInterval(() => {
+        setTime((prevTime) => (prevTime > 0 ? prevTime - 1 : 0));
+      }, 1000);
+    });
+  };
+
   return <div></div>;
 }
 
