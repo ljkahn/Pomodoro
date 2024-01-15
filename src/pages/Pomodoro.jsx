2fa803f50c0a7b2d9832080cd269bee13e277f67
ljkahn <lia.j.kahn@gmail.com>
2024-01-15 16:34:50 -0700

    added increase/decrease break time buttons

--- src/pages/Pomodoro.jsx
+++ src/pages/Pomodoro.jsx
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from "react";
-
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
 function Pomodoro() {
   const [time, setTime] = useState(25 * 60);
   const [isActive, setIsActive] = useState(false);
@@ -13,18 +17,39 @@ function Pomodoro() {
     ).padStart(2, "0")}`;
   };
 
-  const startTimer = () => {
-    setIsActive(true);
+  //normal session clock
+  const toggleTimer = () => {
+    setIsActive((prevIsActive) => !prevIsActive);
   };
+  // const startTimer = () => {
+  //   setIsActive(true);
+  // };
 
-  const stopTimer = () => {
-    setIsActive(false);
-  };
+  // const stopTimer = () => {
+  //   setIsActive(false);
+  // };
 
   const resetTimer = () => {
     setIsActive(false);
     setIsBreak(false);
     setTime(25 * 60);
+  };
+
+  const increaseTime = () => {
+    setTime((prevTime) => prevTime + 1 * 60);
+  };
+
+  const decreaseTime = () => {
+    setTime((prevTime) => prevTime - 1 * 60, 0);
+  };
+
+  //break clock
+  const increaseBreak = () => {
+    setBreakTime((prevBreakTime) => prevBreakTime + 1 * 60);
+  };
+
+  const decreaseBreak = () => {
+    setBreakTime((prevBreakTime) => prevBreakTime - 1 * 60, 0);
   };
 
   useEffect(() => {
@@ -54,19 +79,49 @@ function Pomodoro() {
   }, [isActive, isBreak, time]);
 
   return (
-    <div>
-      <h1>Pomodoro Timer</h1>
-      <p>{formatTime(time)}</p>
-      <button onClick={startTimer} disabled={isActive}>
-        Start Timer
-      </button>
-      <button onClick={stopTimer} disabled={!isActive}>
-        Stop Timer
-      </button>
-      <button onClick={resetTimer} disabled={isActive}>
-        Reset Timer
-      </button>
-    </div>
+    <>
+      <h1 className="head">Pomodoro Clock</h1>
+      <Container className="cardContainer d-flex align-items-center justify-content-center min-vh-100">
+        <Row>
+          <Col md={6}>
+            <Card style={{ width: "30rem", height: "25rem" }} className="card">
+              {/* <Card.Img variant="top" src="" /> */}
+              <Card.Body>
+                <Card.Title>Start Session</Card.Title>
+                <Card.Text>{formatTime(time)}</Card.Text>
+
+                <Button variant="dark" onClick={toggleTimer}>
+                  {isActive ? "Pause Timer" : "Start Timer"}
+                </Button>
+
+                <Button variant="dark" onClick={resetTimer} disabled={isActive}>
+                  Reset Timer
+                </Button>
+
+                <div>
+                  <Button variant="dark" onClick={increaseTime}>
+                    Increase Time
+                  </Button>
+                  <Button variant="dark" onClick={decreaseTime}>
+                    Decrease Time
+                  </Button>
+                </div>
+
+                <div>
+                  <Button variant="dark" onClick={increaseBreak}>
+                    Increase Break Time
+                  </Button>
+                  <Button variant="dark" onClick={decreaseBreak}>
+                    Decrease Break Time
+                  </Button>
+                  <Button variant="outline-light">Light</Button>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
 
