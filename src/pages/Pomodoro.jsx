151fcdf73fbf9a2c27d2c1b2c6a2d5b3fb8da344
ljkahn <lia.j.kahn@gmail.com>
2024-02-09 13:08:13 -0700

    this is a commit message

--- src/pages/Pomodoro.jsx
+++ src/pages/Pomodoro.jsx
@@ -4,8 +4,10 @@ import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
-
-import Stack from "@mui/material/Stack";
+import RestartAltIcon from "@mui/icons-material/RestartAlt";
+import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
+import CircularProgress from "@mui/material/CircularProgress";
 import Button from "@mui/material/Button";
 
 function Pomodoro() {
@@ -16,6 +18,14 @@ function Pomodoro() {
   // Break session state
   const [breakTime, setBreakTime] = useState(5 * 60);
   const [isBreakActive, setIsBreakActive] = useState(false);
+
+  const calculateBizProgress = () => {
+    return (workTime / (25 * 60)) * 100; // Calculate progress percentage
+  };
+
+  const calculateBreakProgress = () => {
+    return (breakTime / (25 * 60)) * 100; // Calculate progress percentage
+  };
 
   const formatTime = (seconds) => {
     const minutes = Math.floor(seconds / 60);
@@ -92,113 +102,131 @@ function Pomodoro() {
   return (
     <>
       <h1 className="head">Pomodoro Clock</h1>
-      <Container className="card-container d-flex align-items-center justify-content-center min-vh-100">
-        <Row>
-          <Col md={6}>
-            <Card style={{ width: "30rem", height: "20rem" }} className="card">
-              <Card.Body>
-                <Card.Title>Business</Card.Title>
-                <Card.Text>{formatTime(workTime)}</Card.Text>
-                <Button
-                  variant="contained"
-                  onClick={toggleWorkTimer}
-                  startIcon={<PlayArrowIcon />}
-                  style={{
-                    backgroundColor: "rgb(173, 158, 163)",
-                    margin: "1rem",
-                  }}
-                >
-                  {isWorkActive ? "Pause Timer" : "Start Timer"}
-                </Button>
-                <Button
-                  variant="contained"
-                  onClick={resetWorkTimer}
-                  disabled={isWorkActive}
-                  style={{
-                    backgroundColor: "rgb(173, 158, 163)",
-                    margin: "1rem",
-                  }}
-                >
-                  Reset Timer
-                </Button>
-                <div>
-                  <Button
-                    variant="contained"
-                    onClick={() => adjustWorkTime(60)}
-                    style={{
-                      backgroundColor: "rgb(173, 158, 163)",
-                      margin: "1rem",
-                    }}
-                  >
-                    ⬆️ Increase Time
-                  </Button>
-                  <Button
-                    variant="contained"
-                    onClick={() => adjustWorkTime(-60)}
-                    style={{
-                      backgroundColor: "rgb(173, 158, 163)",
-                      margin: "1rem",
-                    }}
-                  >
-                    ⬇️ Decrease Time
-                  </Button>
-                </div>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col md={6}>
-            <Card style={{ width: "30rem", height: "20rem" }} className="card">
-              <Card.Body>
-                <Card.Title>Break</Card.Title>
-                <Card.Text>{formatTime(breakTime)}</Card.Text>
-                <Button
-                  variant="contained"
-                  onClick={toggleBreakTimer}
-                  startIcon={<PlayArrowIcon />}
-                  style={{
-                    backgroundColor: "rgb(173, 158, 163)",
-                    margin: "1rem",
-                  }}
-                >
-                  {isBreakActive ? "Pause Timer" : "Start Timer"}
-                </Button>
-                <Button
-                  variant="contained"
-                  onClick={resetBreakTimer}
-                  disabled={isBreakActive}
-                  style={{
-                    backgroundColor: "rgb(173, 158, 163)",
-                    margin: "1rem",
-                  }}
-                >
-                  Reset Timer
-                </Button>
-                <div>
-                  <Button
-                    variant="contained"
-                    onClick={() => adjustBreakTime(60)}
-                    style={{
-                      backgroundColor: "rgb(173, 158, 163)",
-                      margin: "1rem",
-                    }}
-                  >
-                    ⬆️ Increase Time
-                  </Button>
-                  <Button
-                    variant="contained"
-                    onClick={() => adjustBreakTime(-60)}
-                    style={{
-                      backgroundColor: "rgb(173, 158, 163)",
-                      margin: "1rem",
-                    }}
-                  >
-                    ⬇️ Decrease Time
-                  </Button>
-                </div>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
+      <Container className="card-container d-flex flex-row align-items-center justify-content-center min-vh-100">
+        <Card style={{ width: "30rem", height: "35rem" }} className="card">
+          <Card.Body>
+            <CircularProgress
+              variant="determinate"
+              value={calculateBizProgress()}
+              size={150}
+              thickness={4}
+              color="inherit"
+              sx={{ marginBottom: "1rem", marginTop: "1rem" }}
+            />
+            <Card.Title>Business</Card.Title>
+            <Card.Text>{formatTime(workTime)}</Card.Text>
+            <Button
+              variant="contained"
+              onClick={toggleWorkTimer}
+              startIcon={<PlayArrowIcon />}
+              style={{
+                backgroundColor: "rgb(173, 158, 163)",
+                margin: "1rem",
+              }}
+            >
+              {isWorkActive ? "Pause Timer" : "Start Timer"}
+            </Button>
+            <Button
+              variant="contained"
+              endIcon={<RestartAltIcon />}
+              onClick={resetWorkTimer}
+              disabled={isWorkActive}
+              style={{
+                backgroundColor: "rgb(173, 158, 163)",
+                margin: "1rem",
+              }}
+            >
+              Reset Timer
+            </Button>
+
+            <div>
+              <Button
+                variant="contained"
+                startIcon={<ArrowUpwardIcon />}
+                onClick={() => adjustWorkTime(60)}
+                style={{
+                  backgroundColor: "rgb(173, 158, 163)",
+                  margin: "1rem",
+                }}
+              >
+                Increase Time
+              </Button>
+              <Button
+                variant="contained"
+                endIcon={<ArrowDownwardIcon />}
+                onClick={() => adjustWorkTime(-60)}
+                style={{
+                  backgroundColor: "rgb(173, 158, 163)",
+                  margin: "1rem",
+                }}
+              >
+                Decrease Time
+              </Button>
+            </div>
+          </Card.Body>
+        </Card>
+
+        <Card style={{ width: "30rem", height: "35rem" }} className="card">
+          <Card.Body>
+            <CircularProgress
+              variant="determinate"
+              value={calculateBreakProgress()}
+              size={150}
+              thickness={4}
+              color="inherit"
+              sx={{ marginBottom: "1rem", marginTop: "1rem" }}
+            />
+            <Card.Title>Break</Card.Title>
+            <Card.Text>{formatTime(breakTime)}</Card.Text>
+            <Button
+              variant="contained"
+              onClick={toggleBreakTimer}
+              startIcon={<PlayArrowIcon />}
+              style={{
+                backgroundColor: "rgb(173, 158, 163)",
+                margin: "1rem",
+              }}
+            >
+              {isBreakActive ? "Pause Timer" : "Start Timer"}
+            </Button>
+            <Button
+              variant="contained"
+              endIcon={<RestartAltIcon />}
+              onClick={resetBreakTimer}
+              disabled={isBreakActive}
+              style={{
+                backgroundColor: "rgb(173, 158, 163)",
+                margin: "1rem",
+              }}
+            >
+              Reset Timer
+            </Button>
+            <div>
+              <Button
+                variant="contained"
+                startIcon={<ArrowUpwardIcon />}
+                onClick={() => adjustBreakTime(60)}
+                style={{
+                  backgroundColor: "rgb(173, 158, 163)",
+                  margin: "1rem",
+                }}
+              >
+                Increase Time
+              </Button>
+              <Button
+                variant="contained"
+                endIcon={<ArrowDownwardIcon />}
+                onClick={() => adjustBreakTime(-60)}
+                style={{
+                  backgroundColor: "rgb(173, 158, 163)",
+                  margin: "1rem",
+                }}
+              >
+                Decrease Time
+              </Button>
+            </div>
+          </Card.Body>
+        </Card>
       </Container>
     </>
   );
